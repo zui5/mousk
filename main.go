53fcc2fd400eca621aaa0004e22e31a7391301dd
bugsdance <is.zhuosheng@outlook.com>
2024-02-25 18:13:21 +0800

    Init project

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"embed"
+	"fmt"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/go-vgo/robotgo"
 )
 
 //go:embed all:frontend/dist
@@ -13,23 +12,30 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	// app := NewApp()
+	testrobotgo()
 
 	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "MouseK",
-		Width:  1024,
-		Height: 768, AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
+	// err := wails.Run(&options.App{
+	// 	Title:  "MouseK",
+	// 	Width:  1024,
+	// 	Height: 768, AssetServer: &assetserver.Options{
+	// 		Assets: assets,
+	// 	},
+	// 	BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+	// 	OnStartup:        app.startup,
+	// 	Bind: []interface{}{
+	// 		app,
+	// 	},
+	// })
 
-	if err != nil {
-		println("Error:", err.Error())
-	}
+	// if err != nil {
+	// 	println("Error:", err.Error())
+	// }
+}
+
+func testrobotgo() {
+	fmt.Println("test robot go")
+	robotgo.Move(500, 20)
+
 }
