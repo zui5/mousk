5df36172c3bd5d9da2cf52f636daaf0e9adf8824
zui5 <is.zhuosheng@outlook.com>
2024-02-29 00:10:00 +0800

    add basic mouse pointor control method

--- go.mod
+++ go.mod
@@ -2,31 +2,6 @@ module changeme
 
 go 1.18
 
-require github.com/go-vgo/robotgo v1.0.0-rc2.1
-
-require (
-	github.com/gen2brain/shm v0.1.0 // indirect
-	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/jezek/xgb v1.1.1 // indirect
-	github.com/kbinani/screenshot v0.0.0-20230812210009-b87d31814237 // indirect
-	github.com/lufia/plan9stats v0.0.0-20231016141302-07b5767bb0ed // indirect
-	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
-	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
-	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
-	github.com/robotn/xgb v0.0.0-20190912153532-2cb92d044934 // indirect
-	github.com/robotn/xgbutil v0.0.0-20190912154524-c861d6f87770 // indirect
-	github.com/shirou/gopsutil/v3 v3.24.1 // indirect
-	github.com/shoenig/go-m1cpu v0.1.6 // indirect
-	github.com/tklauser/go-sysconf v0.3.13 // indirect
-	github.com/tklauser/numcpus v0.7.0 // indirect
-	github.com/vcaesar/gops v0.30.2 // indirect
-	github.com/vcaesar/imgo v0.40.0 // indirect
-	github.com/vcaesar/keycode v0.10.1 // indirect
-	github.com/vcaesar/tt v0.20.0 // indirect
-	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/image v0.15.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-)
+require golang.org/x/sys v0.17.0
 
 // replace github.com/wailsapp/wails/v2 v2.8.0 => C:\Users\x\.g\gopath\pkg\mod
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"changeme/base"
 	"embed"
 	"fmt"
-
-	"github.com/go-vgo/robotgo"
+	"sync"
+	"time"
 )
 
 //go:embed all:frontend/dist
@@ -13,7 +14,8 @@ var assets embed.FS
 func main() {
 	// Create an instance of the app structure
 	// app := NewApp()
-	testrobotgo()
+	// testrobotgo()
+	testwin32()
 
 	// Create application with options
 	// err := wails.Run(&options.App{
@@ -35,7 +37,50 @@ func main() {
 }
 
 func testrobotgo() {
-	fmt.Println("test robot go")
-	robotgo.Move(500, 20)
 
+	var wg sync.WaitGroup
+	fmt.Println("--- Please press ctrl + shift + q to stop hook ---")
+	wg.Add(1)
+	// hook.Register(hook.KeyDown, []string{"q", "ctrl", "shift"}, func(e hook.Event) {
+	// 	// defer wg.Done()
+	// 	// wg.Add(1)
+	// 	fmt.Println("ctrl-shift-q")
+	// 	hook.End()
+	// })
+	// fmt.Println("--- Please press w---")
+	// hook.Register(hook.KeyDown, []string{"1"}, func(e hook.Event) {
+	// 	locx, locy := robotgo.Location()
+	// 	fmt.Println("before:" + fmt.Sprint(locx) + "," + fmt.Sprint(locy))
+	// 	// robotgo.Move(locx, locy+10)
+	// 	robotgo.MoveMouse(locx, locy+1)
+	// 	locx, locy = robotgo.Location()
+	// 	fmt.Println("after:" + fmt.Sprint(locx) + "," + fmt.Sprint(locy))
+
+	// })
+
+	// s := hook.Start()
+	// <-hook.Process(s)
+	wg.Wait()
+	// robotgo.Move(1200, 20)
+	// robotgo.DragSmooth(700, 200)
+
+}
+
+func testwin32() {
+	// 获取当前鼠标位置
+	currentX, currentY := base.GetMousePos()
+	fmt.Printf("Current Mouse Position: (%d, %d)\n", currentX, currentY)
+
+	base.SetMousePos(1475, 800)
+	base.MoveMouse(-100, 0)
+
+	// 等待一段时间，以便观察鼠标移动
+	time.Sleep(3 * time.Second)
+
+	// 获取移动后的鼠标位置
+	newX, newY := base.GetMousePos()
+	fmt.Printf("New Mouse Position: (%d, %d)\n", newX, newY)
+
+	// 恢复鼠标原始位置
+	base.SetMousePos(currentX, currentY)
 }
