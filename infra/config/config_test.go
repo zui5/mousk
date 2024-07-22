b29be255eca7f6f4f98f8edbd92d29c1325ecb7b
bugsdance <is.zhuosheng@outlook.com>
2024-07-22 22:30:57 +0800

    新增配置初始化

--- infra/config/config_test.go
+++ infra/config/config_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestInit(t *testing.T) {
 	Init()
-	settingsVar.Mouse.Fast.Speed = 1000
+	// settingsVar.Mouse.Fast.Speed = 1000
+	settingsVar.PresetFunc.MouseMove.SpeedLevel.Level1 = -1
 	WriteSettings()
 	RestoreSettings()
 }
